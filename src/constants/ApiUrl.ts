cc6304b312fc902a09532ba44d305743c6650f99
Tomasz <tomasz9531@gmail.com>
2021-09-03 14:48:16 +0200

    Set production api url

--- src/constants/ApiUrl.ts
+++ src/constants/ApiUrl.ts
@@ -1,5 +1,5 @@
 const LOCAL_API_URL = process.env.NEXT_PUBLIC_LOCAL_API_URL;
-const API_CORE = LOCAL_API_URL ? LOCAL_API_URL : 'https://api.famechallenge.pl';
+const API_CORE = LOCAL_API_URL ? LOCAL_API_URL : 'http://fummy.co:8000';
 
 export const API_URL = API_CORE + '/api/';
 export const API_STORAGE = API_CORE + '/storage/';
