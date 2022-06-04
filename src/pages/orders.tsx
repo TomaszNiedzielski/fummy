72c27dbbf01dc5abbbbac1aa6ea2c1f2d00c3149
Tomasz <tomasz9531@gmail.com>
2022-06-04 14:31:00 +0200

    Set interval to update orders

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import OrderCard from '../components/dedicated/orders/card/Card';
 import { get, post, Response } from '../helpers/ApiRequest';
 import Cookies from 'universal-cookie';
 import Navigator from '../components/dedicated/orders/navigator/Navigator';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
 
 const CurrentOrders = ({ orders }) => {
     if(orders.length > 0) {
@@ -79,7 +81,26 @@ const UnrealizedOrders = ({ orders }) => {
 
 export type OrdersView = 'current' | 'completed' | 'uncompleted';
 
-const OrdersPage = ({ orders }) => {
+const OrdersPage = ({ ordersProps }) => {
+    const [orders, setOrders] = useState(ordersProps);
+    const { token } = useSelector((state: RootState) => state.auth);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            get('orders?token='+token)
+            .then((response: Response) => {
+                if(response.code === 200) {
+                    setOrders(response.data);
+                }
+            });
+
+            post('notifications/mark-as-read?token='+token);
+        }, 10000);
+        
+        return () => clearInterval(interval);
+    }, [token]);
+
+
     return (
         <div className="container mt-5 mt-md-0 mx-0 px-0 d-flex flex-column flex-md-row mw-100">
             <Router>
@@ -126,7 +147,7 @@ export const getServerSideProps = async ({ req }) => {
 
     if(orders) {
         return {
-            props: { orders }
+            props: { ordersProps: orders }
         }
     }
 }
